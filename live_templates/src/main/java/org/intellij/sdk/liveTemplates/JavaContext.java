c3b6aea773ee63122d0b25afe3e7c27f8eb63acd
DaVicki <vicky.jha24dev@gmail.com>
2020-12-12 20:08:57 -0700

    add live template to log method name

---
+++ live_templates/src/main/java/org/intellij/sdk/liveTemplates/JavaContext.java
@@ -0,0 +1,20 @@
+// Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.intellij.sdk.liveTemplates;
+
+import com.intellij.codeInsight.template.TemplateActionContext;
+import com.intellij.codeInsight.template.TemplateContextType;
+import org.jetbrains.annotations.NotNull;
+
+public class JavaContext extends TemplateContextType {
+
+    protected JavaContext() {
+        super("JAVA", "Java");
+    }
+
+    @Override
+    public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
+        return templateActionContext.getFile().getName().endsWith(".java");
+    }
+
+}
