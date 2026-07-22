348f522a7f77ec69b85e538dbe2cb979d986d574
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-07-22 15:22:51 +0200

    Add custom testWithoutVue test task as a workaround to NoClassDefFoundError

--- comparing_string_references_inspection/build.gradle.kts
+++ comparing_string_references_inspection/build.gradle.kts
@@ -1,4 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
@@ -16,4 +17,16 @@ dependencies {
   }
 
   testImplementation("junit:junit:4.13.2")
+}
+
+// workaround for the Vue plugin causing test failure due to NoClassDefFoundError
+// when fixed, delete this and `if` in workflow code-samples.yml
+val testWithoutVue by intellijPlatformTesting.testIde.register("testWithoutVue") {
+  type = IntelliJPlatformType.IntellijIdea
+
+  testFramework(TestFrameworkType.Platform)
+  testFramework(TestFrameworkType.Plugin.Java)
+  plugins {
+    disablePlugin("org.jetbrains.plugins.vue")
+  }
 }
